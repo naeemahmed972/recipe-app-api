c1ad88afc1da9df8fa38df4f6e462ac6bd1682da
Naeem Ahmed <naeem.ahmed972@gmail.com>
2023-10-23 21:02:20 +0500

    Added the custom user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_sccessful(self):
+        email = 'test@example.com'
+        password = 'Abcd@1234'
+
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'Abcd@1234')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'Abcd@1234')
+
+    def test_create_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'Abcd@1234'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
